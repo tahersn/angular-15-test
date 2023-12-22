b3a8ccc773d5edf3fad5e4fe2708c354f4e67ce4
Taher <taher.snoussi@esprit.tn>
2023-12-22 16:55:24 +0100

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '', pathMatch: 'full'},
+  {path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule)},
+  {path: '**', redirectTo: 'products', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ProductsModule} from './products/products.module';
+import { Router, RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ProductsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
